e54dfdaee945b99ef67a85f5cb4b706c86df5702
Jackson Dou <jksdou@qq.com>
2023-06-08 17:29:06 +0800

    update: 移除多余功能

--- src/user/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer text-center">
-      <div class="container">
-        <p class="text-muted">Copyright &copy; <?php echo date('Y')?> <a href="/"><?php echo $conf['title']?></a> <?php echo $conf['tongji']?> </p>
-      </div>
-    </footer>
-<script src="//cdn.staticfile.org/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
